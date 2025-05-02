c2935614b1418972a623c3ba984df8a20aede8a6
henry <luiz03hamer@gmail.com>
2025-05-02 06:23:42 -0300

    atualizar numero

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ function enviarMensagem() {
     if (sala) mensagem += `• Sala de velório: ${sala}%0A`;
   }
 
-  const telefone = "(11)97603528";
+  const telefone = "(11)976035628";
   window.open(`https://wa.me/${telefone}?text=${mensagem}`, "_blank");
 }
 
